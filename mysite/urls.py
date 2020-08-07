f2ed351c75bafb06c5b2e7a81f81b717891f4fd4
Rahul <rahular60@gmail.com>
2020-08-07 22:51:19 +0530

    include template

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
     path('',include('main.urls')),
     path('admin/', admin.site.urls),
 ]
